e70c8a416baf13ab479a6cd799f3858f4f5ca631
Harald Nezbeda <hn@nezhar.com>
2025-04-12 15:58:17 +0200

    Create models and migrations for cars

--- carstack/cars/models.py
+++ carstack/cars/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    founded_year = models.IntegerField(null=True, blank=True)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='cars')
+    model_name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    BODY_TYPE_CHOICES = [
+        ('sedan', 'Sedan'),
+        ('suv', 'SUV'),
+        ('hatchback', 'Hatchback'),
+        ('coupe', 'Coupe'),
+        ('convertible', 'Convertible'),
+        ('truck', 'Truck'),
+        ('van', 'Van'),
+    ]
+    body_type = models.CharField(max_length=20, choices=BODY_TYPE_CHOICES)
+    
+    is_electric = models.BooleanField(default=False)
+    color = models.CharField(max_length=50)
+    mileage = models.IntegerField(default=0)
+    features = models.ManyToManyField(Feature, blank=True)
+    
+    def __str__(self):
+        return f"{self.year} {self.manufacturer.name} {self.model_name}"
